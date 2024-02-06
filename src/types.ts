3fdaa88972507fa5439231f3b4b77d4b789f0771
Ben Watkins <hi@bwatkins.dev>
2024-02-06 01:11:16 -0600

    fixups on receipt model

--- src/types.ts
+++ src/types.ts
@@ -17,22 +17,27 @@ export type Storage = {
   receipts: Map<string, ProcessedReceipt>;
 };
 
+// Deviation: Accept numbers and strings. Be on the lookout for number truncation.
 const money = z.coerce.number().positive().finite();
 export const receiptSchema = z.object({
   // Deviation: I added `&` to allow provided examples
   // ref https://github.com/fetch-rewards/receipt-processor-challenge/pull/11#issuecomment-1927953077
   retailer: z.string().regex(/^[\w\s-&]+$/),
-  // TODO: check against yaml spec
+  // I'm just gonna let anything close enough through here. Logic handling dates is tested to support invalid dates.
+  // Probably want to figure out why we're not using a combined date/time
   purchaseDate: z.string().regex(/\d{4}-\d{2}-\d{2}/),
-  // TODO: check against yaml spec
+  // Deviation: I'm not allowing seconds even though YAML's `format: time` can do 12/24 hour with seconds
+  // Deviation: Let's invalid times through. Logic handling times knows what to do with it.
+  //            Consider full date/time as a fix to avoid writing parsing logic?
   purchaseTime: z.string().regex(/\d{2}:\d{2}/),
-  // TODO: does not strictly match spec
   total: money,
-  items: z.array(
-    z.object({
-      shortDescription: z.string().regex(/^[\w\s-]+$/),
-      price: money,
-    }),
-  ),
+  items: z
+    .array(
+      z.object({
+        shortDescription: z.string().regex(/^[\w\s-]+$/),
+        price: money,
+      }),
+    )
+    .min(1),
 });
 export type Receipt = z.infer<typeof receiptSchema>;
